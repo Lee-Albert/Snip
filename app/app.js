c9ce6e4b9ea25d2b2ccc646d01bd5eefc8984224
alexshi0000 <alexshi0000@gmail.com>
2019-02-02 15:09:18 -0500

    routing

--- app/app.js
+++ app/app.js
@@ -1,1 +1,15 @@
-console.log("working EKIM");
+var path    = require('path')
+var express = require('express')
+var app     = express()
+var http    = require('http').Server(app)
+var io      = require('socket.io')(http)
+var fs      = require('fs')
+
+//endpoint
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/html/register.html');
+});
+
+app.get('/public/css/register.css', (req, res) => {
+  res.sendFile(__dirname + '/public/css/register.css')
+});
