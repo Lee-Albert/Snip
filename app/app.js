e8e1d26684f76e2ac2ca756f1c2cf274cc8acfa3
alexshi0000 <alexshi0000@gmail.com>
2019-02-03 02:41:26 -0500

    big commit

--- app/app.js
+++ app/app.js
@@ -4,6 +4,9 @@ var app     = express()
 var http    = require('http').Server(app)
 var io      = require('socket.io')(http)
 var fs      = require('fs')
+var url = "mongodb://localhost:27017/";
+var MongoClient = require('mongodb').MongoClient;
+
 
 //endpoint
 app.get('/', (req, res) => {
@@ -26,15 +29,59 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 
+// ===============================================================
+// print the in coifs
+app.get('/api/get/coifs', (req, res) => {
+  MongoClient.connect(url, (err, db) => {
+    if (err)
+      throw err;
+    var dbo = db.db('snippdb');
+
+    dbo.collection('coifs').find({}, {projection: { _id : 0 }}).toArray((err, result) => {
+      if (err)
+        throw err;
+      if (result.length != 0) {
+        console.log('found all');
+        res.status(200).json(result);
+      }
+      else {
+        console.log('no coifs');
+        res.status(400).json( { error:"no coifs found" } );
+      }
+      db.close();
+    });
+  });
+});
 
 app.post('/api/coif-login', (req, res) => {
-  console.log('COIF TRIED TO LOGIN');
+  console.log('new coif login');
   console.log('coif name: ' + req.body.name);
   console.log('coif email: ' + req.body.email);
+
+  MongoClient.connect(url, (err, db) => {
+    if (err)
+      return err;
+    var dbo = db.db('snippdb');
+    var new_coif = {
+      name  : req.body.name,
+      email : req.body.email
+    };
+    dbo.collection('coif').insertOne(new_coif, (err, res) => {
+      if (err) {
+        res.status(400).json({ error:"error something oops" });
+        throw err;
+      }
+      console.log("added coif success");
+      res.status(200).json({ coif:new_coif });
+      db.close();
+    });
+  });
 });
 
 app.post('/api/client-login', (req, res) => {
-  console.log('CLIENT TRIED TO LOGIN');
+  console.log('new client login');
+  console.log('client name: ' + req.body.name);
+  console.log('client email: ' + req.body.email);
 });
 
 http.listen(3000, () => {
---
+++ app/init_db.js
@@ -0,0 +1,33 @@
+var path        = require('path')
+var MongoClient = require('mongodb').MongoClient;
+var url         = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, (err, db) => {
+  if (err)
+    throw err;
+  var fs = require('fs');
+  var text = JSON.parse(fs.readFileSync(__dirname + '/databaseinfo.json', 'utf8'));
+  var coifs = text.coiffeurs;
+  var clients = text.customers;
+
+  var dbo = db.db("snippdb");
+
+  dbo.collection("coif").insertMany(coifs, (err, res) => {
+    if (err)
+      throw err;
+    console.log("Number of documents inserted: " + res.insertedCount);
+    console.log(res.insertedIds);
+    console.log(res.ops);
+    console.log(res.result);
+
+    dbo.collection("client").insertMany(clients, (err, res) => {
+      if (err)
+        throw err;
+      console.log("Number of documents inserted: " + res.insertedCount);
+      console.log(res.insertedIds);
+      console.log(res.ops);
+      console.log(res.result);
+    });
+    db.close();
+  });
+});
